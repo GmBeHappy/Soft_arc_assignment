86185e3bdb2c3632b3fe7f0d9bf1c71af908d5ea
Witchayut Gerdchai <gmplk@hotmail.com>
2022-10-12 15:00:43 +0700

    first commit

---
+++ ex1/src/Square.java
@@ -0,0 +1,18 @@
+// package com.solid.lsp;
+
+public class Square implements Shape {
+    private int side;
+
+    Square(int side) {
+        this.setSide(side);
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public int getArea() {
+        return side * side;
+    }
+}
